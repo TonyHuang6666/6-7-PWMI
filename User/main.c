e179601ff8407b5836b204937933d750ed9922f5
Tony_Huang <Tony_Huang@TONY_HUANG666>
2023-07-28 17:25:54 +0800

    测试通过

--- User/main.c
+++ User/main.c
@@ -8,15 +8,15 @@ int main(void)
 	OLED_Init();
 	PWM_Init();
 	IC_Init();
-	PWM_SetDuty(50); //duty=CCR/(ARR+1),ARR=100-1
+	PWM_SetDuty(90); //duty=CCR/(ARR+1),ARR=100-1
 	PWM_SetPrescaler(14400-1);  //Frequency=72MHz/(PSC+1)/(ARR+1)=100Hz,ARR=100-1
 	OLED_ShowString(1, 1, "Freq:");
 	OLED_ShowString(1, 13, "Hz");
 	OLED_ShowString(2, 1, "Duty:");
-	OLED_ShowString(2, 9, "%");
+	OLED_ShowString(2, 8, "%");
 	while (1)
 	{
 		OLED_ShowNum(1, 6, Get_Frequency(), 7);
-		OLED_ShowNum(2, 6, Get_Duty(), 3);
+		OLED_ShowNum(2, 6, Get_Duty(), 2);
 	}
 }
